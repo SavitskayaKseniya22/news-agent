7fc6c2e049cf4f7b1d78716f0c3e1228c9c272e6
SavitskayaKseniya22 <savitskaya.kseniya.22@yandex.ru>
2024-04-03 01:32:42 +0300

    feat: add endpoints to newsApi

--- src/app/store/hackerNewsApi.tsx
+++ src/app/store/hackerNewsApi.tsx
@@ -10,7 +10,28 @@ export const hackerNewsApi = createApi({
     getTopStories: builder.query<number[], void>({
       query: () => ({ url: `/topstories.json?print=pretty`, method: 'GET' }),
     }),
+    getNewStories: builder.query<number[], void>({
+      query: () => ({ url: `/newstories.json?print=pretty`, method: 'GET' }),
+    }),
+    getBestStories: builder.query<number[], void>({
+      query: () => ({ url: `/beststories.json?print=pretty`, method: 'GET' }),
+    }),
+    getAskStories: builder.query<number[], void>({
+      query: () => ({ url: `/askstories.json?print=pretty`, method: 'GET' }),
+    }),
+    getShowStories: builder.query<number[], void>({
+      query: () => ({ url: `/showstories.json?print=pretty`, method: 'GET' }),
+    }),
+    getJobStories: builder.query<number[], void>({
+      query: () => ({ url: `/jobstories.json?print=pretty`, method: 'GET' }),
+    }),
     getStory: builder.query<ContentDetailesType, { id: number }>({
+      query: ({ id }) => ({
+        url: `item/${id}.json?print=pretty`,
+        method: 'GET',
+      }),
+    }),
+    getUser: builder.query<ContentDetailesType, { id: number }>({
       query: ({ id }) => ({
         url: `item/${id}.json?print=pretty`,
         method: 'GET',
@@ -19,4 +40,13 @@ export const hackerNewsApi = createApi({
   }),
 });
 
-export const { useGetTopStoriesQuery, useGetStoryQuery } = hackerNewsApi;
+export const {
+  useGetTopStoriesQuery,
+  useGetStoryQuery,
+  useGetNewStoriesQuery,
+  useGetBestStoriesQuery,
+  useGetAskStoriesQuery,
+  useGetShowStoriesQuery,
+  useGetJobStoriesQuery,
+  useGetUserQuery,
+} = hackerNewsApi;
